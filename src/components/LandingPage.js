fdde9c05d8cbf5c4312b770d7467133660e20107
amarnath <amarnathmandal097@gmail.com>
2021-12-08 18:08:57 +0530

    changed landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,27 +1,70 @@
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
-import 'aos/dist/aos.css'
-import Aos from 'aos'
 
 const LandingPage = () => {
-    useEffect(() => {
-        Aos.init({ duration: 700 })
-    })
+
+    const [showPopUp, setShowPopUp] = useState(false);
+
+    const styels = {
+        position: 'absolute',
+        height: '8.4rem',
+        width: '8.4rem',
+        borderRadius: '50%',
+        right: '4.8rem',
+        bottom: ' 4.7rem',
+        zIndex: '3'
+
+    }
+    const styels2 = {
+        position: ' absolute',
+        height: '100%',
+        width: '100%',
+        border: 'none',
+        transition: ' all 1s',
+    }
+    const [s, setS] = useState(styels);
+
+    const handleClick = () => {
+        switch (showPopUp) {
+            case false:
+                setShowPopUp(true);
+                setS(styels2)
+                break;
+            case true:
+                setShowPopUp(false);
+                setS(styels)
+                break;
+            default:
+                setShowPopUp(false);
+                setS(styels2)
+        }
+    }
+
+
     return (
         <>
             <Container >
-                {/* <div> */}
-                <Img data-aos="fade-up" data-aos-offset="100" data-aos-easing="linear" src='https://offbeat.qodeinteractive.com/wp-content/uploads/2018/04/landing-slider-img-3.png' />
-                <Iframe data-aos="fade-up" data-aos-offset="100" data-aos-easing="linear" src="https://www.youtube.com/embed/k4ixAfJ1LuI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></Iframe>
-                {/* </div> */}
+                <div className="after" onClick={handleClick} ></div>
+                <Logo src='Images/missingnotelogo.jpeg' />
+                <CircleDiv >
+                    <Img src="Images/watchbtn.jpg" alt="" />
+                </CircleDiv>
+                <Iframe onClick={handleClick}
+                    src={`Video/tmnconcert.mp4?autoplay=1`}
+                    autoplay
+                    style={s}
+                    allowfullscreen
+                >
+                </Iframe>
             </Container>
         </>
     )
 }
 
+
 const Container = styled.div`
 height: 100vh;
-background:url('https://media.istockphoto.com/photos/web-banner-hobby-and-lifestyle-activity-in-holiday-from-beauty-asian-picture-id1167491954?b=1&k=20&m=1167491954&s=170667a&w=0&h=Si5YT6bZ7aYnR3hCAJ9MRyVWG2IkkA36nfBE_-lcSAs=');
+background:linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.8)) , url('Images/missingnote.jpeg');
 background-repeat: no-repeat;
 object-fit:cover;
 width: 100%;
@@ -29,17 +72,55 @@ background-size: cover;
 display: flex;
 align-items: center;
 justify-content: center;
+position: relative;
+overflow-y: hidden;
+.after{
+    content: "";
+    position: absolute;
+    height: 13rem;
+    width: 13rem;
+    border-radius: 50%;
+    cursor: pointer;
+    bottom: 3rem;
+    right: 3rem;
+    z-index:2;
+}
+
 `
-const Img = styled.img`
-height: 45vh ;
-width: 38vw;
+const Logo = styled.img`
+height: auto ;
+width: 20rem;
+object-fit: contain;
+position: absolute;
+bottom: 6rem;
+left: 2rem;
+`
+const CircleDiv = styled.div`
+height: 100vh;
+width: 100%; 
 position: relative;
 `
-const Iframe = styled.iframe`
+
+const Img = styled.img`
+height: 12rem;
+width: 12rem;
 position: absolute;
-height: 36vh;
-width: 28vw;
-border: none;
+bottom:3rem;
+right: 3rem;
+border-radius: 50%;
+object-fit: cover;
+opacity: 0.9;
+filter: contrast(1.2);
+cursor: pointer;
+z-index: 1;
+animation: watchbtn 6s linear infinite;
 `
 
+const Iframe = styled.iframe`
+position: absolute;
+height: 100%;
+width: 100%;
+border: none;
+transition: all 1s;
+`
 export default LandingPage
